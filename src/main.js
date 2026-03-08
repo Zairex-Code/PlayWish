68debd1e5c0c98eeb590f4785083598adb412e69
Zairex-Code <dylanyesidflorez@gmail.com>
2026-03-07 21:18:05 -0500

    Feature/ Navbar and dashboard background added

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import navbarHTML from './layouts/navbar.html?raw'; // vite brings HTML Files as strings only adding "?raw"
+import backgroundDashboardHTML from './layouts/background-dashboard.html?raw';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
 
-setupCounter(document.querySelector('#counter'))
+document.addEventListener('DOMContentLoaded', () => {
+  //1. Inject the navbar
+
+  // We search for the container by its class (class="navbar-content" in index.html)
+  const navbarContainer = document.querySelector('.navbar-content');
+
+  // If the container exists on the current page, we inject the HTML inside it
+  if (navbarContainer) {
+    navbarContainer.innerHTML = navbarHTML;
+  }
+
+  // 2. inject the background image
+  // We search for the container by its class (class="background-dashboard" in index.html)
+  const BackGroundContainer = document.querySelector('.background-dashboard');
+  // If the container exists on the current page, we inject the HTML inside it
+  if (BackGroundContainer){
+    BackGroundContainer.innerHTML = backgroundDashboardHTML;
+  }
+});
