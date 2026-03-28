a34f27dc630e2b060708b6606429030c97c6fb8f
AdrianMatias58 <matihu.919adrian@gmail.com>
2026-03-28 16:55:21 -0500

    Apartado de js para WhisList

---
+++ src/layouts/WhisList/ObjGlobalG.js
@@ -0,0 +1,13 @@
+//objeto gobal de juegos
+const WishList = (()=>{
+    const List ={
+        _games : {}
+    }
+
+    return{
+        set: (id, game) =>List._games[id] = {...game, updatedAt : new Date()},
+        get: (id) => List._games[id] || {message : "Juego descontinuado o no encontrado"},
+        getAll :() => Object.values(List._games)
+    }
+})
+export default WishList
